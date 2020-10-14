8aba8397f58dd7dd556bbecde710ceee48bdb355
rmarzocchi84 <roberto.marzocchi@gter.it>
2020-10-14 13:58:38 +0200

    Aggiunte correzioni alla query sql: subset della rete e direzione in output

--- path3d.py
+++ path3d.py
@@ -15,7 +15,7 @@ import logging
 logging.basicConfig(
     format='%(asctime)s\t%(levelname)s\t%(message)s',
     #filename='log/path3d.log',   #mancano permessi
-    level=logging.DEBUG)
+    level=logging.INFO)
 
 logging.info('*'*20 + ' NUOVA ESECUZIONE ' + '*'*20)
 
@@ -40,9 +40,11 @@ def main():
     if os.path.isfile('{}/tmp/output3d.zip'.format(outdir0)):       
         os.remove('{}/tmp/output3d.zip'.format(outdir0))
 
+
+    # Connect to an existing database
     try:
-        # Connect to an existing database
-        con = psycopg2.connect(host=host, port=port, dbname=dbname, user=user, password=password)
+        
+        con = psycopg2.connect(host=host, port=port, dbname=dbname, user=user, password=password, **keepalive_kwargs)
         logging.info("connected!")
     except psycopg2.Error as e:
         logging.error("unable to connect")
@@ -53,8 +55,9 @@ def main():
     cur = con.cursor()
     
     # First DB query
-    logging.info('Import input points')
+    logging.info('Query 1 - Importing input points')
     query1 = """
+        -- Clean schema
         DROP TABLE IF EXISTS output2 ;
         DROP TABLE IF EXISTS output ;
         DROP INDEX IF EXISTS network_source_idx;
@@ -62,9 +65,11 @@ def main():
         DROP TABLE IF EXISTS lines_split;
         DROP TABLE IF EXISTS points_snapped;
         DROP TABLE IF EXISTS ways_densified;
+        DROP VIEW IF EXISTS ways_subset;
         DROP TABLE IF EXISTS points_over_lines;
         DROP TABLE IF EXISTS input_points;
-
+        
+        -- Define input geometry
         CREATE TABLE input_points (id SERIAL PRIMARY KEY);
         SELECT AddGeometryColumn('', 'input_points', 'geom', 4326, 'POINT', 2);
         CREATE INDEX input_points_geom_idx ON input_points USING gist (geom);
@@ -84,21 +89,30 @@ def main():
         os._exit(1)
 
     # Second DB query 
-    logging.info('Query 2')
+    logging.info('Query 2 - Snapping points to ways')
     query2 = """
+        -- Subset network  
+        CREATE VIEW ways_subset AS
+        SELECT *
+        FROM ways a, (SELECT St_buffer(ST_Collect(geom), 0.2) as geom FROM input_points) b
+        WHERE a.the_geom && b.geom;    
+        
+        -- Find closest points
         CREATE TABLE points_over_lines
         AS SELECT a.id,ST_ClosestPoint(ST_Union(b.the_geom), a.geom)::geometry(POINT,4326) AS geom
-        FROM input_points a, network.ways b
+        FROM input_points a, ways_subset b
         GROUP BY a.geom,a.id;
 
+        -- Densify network 
         CREATE OR REPLACE FUNCTION ST_AsMultiPoint(geometry) RETURNS geometry AS
         'SELECT ST_Union((d).geom) FROM ST_DumpPoints($1) AS d;'
         LANGUAGE sql IMMUTABLE STRICT COST 10;
 
         CREATE TABLE ways_densified AS
         SELECT 1 AS id, ST_Union(ST_AsMultiPoint(st_segmentize(the_geom,0.00001)))::geometry(MULTIPOINT,4326) AS geom 
-        FROM network.ways;
-
+        FROM ways_subset;
+        
+        --Snap points to network
         CREATE TABLE points_snapped AS
         SELECT b.id, ST_snap(ST_Union(b.geom),a.geom, 0.000001)::geometry(POINT,4326) AS geom 
         FROM ways_densified a, points_over_lines b
@@ -114,14 +128,16 @@ def main():
         os._exit(1)
 
     # Third DB query  
-    logging.info('Query 3')
+    logging.info('Query 3 - Finding shortest path')
     query3 = """
 
+        -- Create network splitted on points snapped
         CREATE TABLE lines_split AS
         SELECT a.osm_id, (ST_Dump(ST_split(st_segmentize(a.the_geom,0.00001),ST_Union(b.geom)))).geom::geometry(LINESTRING,4326) AS geom 
-        FROM network.ways a, points_snapped b
+        FROM ways_subset a, points_snapped b
         GROUP BY a.osm_id, a.the_geom;
 
+        -- Recreate topology
         ALTER TABLE lines_split ADD COLUMN idk serial PRIMARY KEY;
         ALTER TABLE lines_split ADD COLUMN source integer;
         ALTER TABLE lines_split ADD COLUMN target integer;
@@ -131,37 +147,63 @@ def main():
         CREATE INDEX network_source_idx ON lines_split("source");
         CREATE INDEX network_target_idx ON lines_split("target");
 
+        -- Defininf cost as length
         ALTER TABLE lines_split ADD COLUMN cost numeric;
         UPDATE lines_split set cost = ST_Length(geom);
 
         ALTER TABLE points_snapped add column idk integer;
 
-        --UPDATE points_snapped p
-        --SET idk = v.id
-        --FROM lines_split_vertices_pgr v
-        --WHERE ST_Contains( v.the_geom, p.geom );
-        
+        -- Find nearest vertex id to snapped points
         UPDATE points_snapped p
         SET idk = (
             SELECT id 
             FROM lines_split_vertices_pgr v
             ORDER BY p.geom <-> v.the_geom LIMIT 1);
 
+        -- Find shortest path with pgr_dijkstra algorithm     
+        CREATE OR REPLACE FUNCTION wrk_dijkstra(
+            OUT seq INTEGER,
+            OUT node BIGINT,
+            OUT edge BIGINT,
+            OUT cost DOUBLE PRECISION,
+            OUT geom geometry)
+            RETURNS SETOF record AS
+            $BODY$
+            WITH
+            dijkstra AS (
+            SELECT * FROM pgr_dijkstra(
+                'SELECT idk AS id, * FROM lines_split',
+                (SELECT idk FROM points_snapped p WHERE p.id = 1),
+                (SELECT idk FROM points_snapped p WHERE p.id = 2),
+                directed := false)
+            ),
+            get_geom AS (
+            SELECT dijkstra.*,
+                -- adjust directionality
+                CASE
+                    WHEN dijkstra.node = lines_split.source THEN geom
+                    ELSE ST_Reverse(geom)
+                END AS route_geom
+            FROM dijkstra JOIN lines_split ON (edge = idk)
+            ORDER BY seq)
+            
+            SELECT seq, node, edge, cost,    
+               route_geom as geom
+               FROM get_geom
+               ORDER BY seq;
+            $BODY$
+            LANGUAGE 'sql';
+
         CREATE TABLE output AS
-        SELECT a.seq, a.node, a.edge, a.cost, b.geom
-        FROM pgr_dijkstra('SELECT idk as id, source, target, cost FROM lines_split',
-                         (SELECT idk FROM points_snapped p WHERE p.id = 1),
-                         (SELECT idk FROM points_snapped p WHERE p.id = 2),
-                          false) AS a  
-        INNER JOIN lines_split b ON (a.edge = b.idk) ORDER BY seq;
-	
+        SELECT * from wrk_dijkstra();
+
     """
     try:
         # Execute the third query
         cur.execute(query3)
         con.commit()
     except psycopg2.Error as e:
-        logging.error('Query3 failed! - unable to find route')
+        logging.error('Query3 failed! - unable to find shortest path')
         logging.error(e.pgerror)
         os._exit(1)
     
@@ -170,7 +212,7 @@ def main():
         logging.error('Empty output table')
         os._exit(1)
     else:
-       logging.info('numero di rige in output {}'.format(cur.rowcount))
+       logging.debug('numero di rige in output {}'.format(cur.rowcount))
     # Close cursor and DB connection
     cur.close()
     con.close()
@@ -178,10 +220,15 @@ def main():
     
     # Save DB output table as geopackage (due to bug in grass connection to DB Postgis )
     logging.info('Ogr2ogr conversion')
-    comando_ogr='/usr/bin/ogr2ogr -f GPKG {}/tmp/output.gpkg PG:"host={} port={} dbname={} user={} password={}" -sql "SELECT * from public.output" -overwrite'.format(outdir0, host, port, dbname, user, password)
-    os.system(comando_ogr)
+    try:
+        comando_ogr='/usr/bin/ogr2ogr -f GPKG {}/tmp/output.gpkg PG:"host={} port={} dbname={} user={} password={}" -sql "SELECT * from public.output" -overwrite'.format(outdir0, host, port, dbname, user, password)
+        os.system(comando_ogr)
+    except:
+        logging.error('Unable to create the output.gpkg file')
+        os._exit(1)
     
     # Define temporary maps name
+    logging.info('Defining GRASS GIS environment')
     global tmp
     tmp = {}
     processid = "{:.2f}".format(time.time())
@@ -227,10 +274,16 @@ def main():
     sys.path.append(grass_pydir)
     
     # import (some) GRASS Python bindings
-    import grass.script as gscript
-    import grass.script.setup as gsetup
+    try:
+        import grass.script as gscript
+        import grass.script.setup as gsetup
+        from grass.exceptions import CalledModuleError
+    except ImportError as e:
+        logging.error(e)
+        os._exit(1)
 
     # Launch grass session
+    logging.info('Launching GRASS GIS session')
     rcfile = gsetup.init(gisbase, gisdb, location, mapset)
     
     # Define main input and output map
@@ -238,38 +291,64 @@ def main():
     dem = 'dem20'
     
     # Set computational region to dem map
+    logging.info('Setting computational region')
     gscript.run_command('g.region', raster=dem, flags='ap')
     
     # Import the geopackage resulting from the DB query
-    gscript.run_command('v.in.ogr', input='{}/tmp/output.gpkg'.format(outdir0), layer='sql_statement', output=line2d, type='line', overwrite=True)
+    logging.info('Importing the query result')
+    if os.path.isfile('{}/tmp/output.gpkg'.format(outdir0)):
+        try:
+            gscript.run_command('v.in.ogr', input='{}/tmp/output.gpkg'.format(outdir0), layer='sql_statement', output=line2d, type='line', overwrite=True, quiet=True)
+        except CalledModuleError as e1:
+            logging.error(e1)
+            os._exit(1)
+    else:
+        logging.error('The output.gpkg file does not exist')
+        os._exit(1)
+        
     
     # Convert 2D geometrie to 3D
-    gscript.run_command('v.drape', input=line2d, type='line', output=tmp["line3d"], elevation=dem, method='bilinear')
+    logging.info('Converting 2D features to 3D')
+    try:
+        gscript.run_command('v.drape', input=line2d, type='line', output=tmp["line3d"], elevation=dem, method='bilinear', quiet=True)
+    except CalledModuleError as e2:
+        logging.error(e2)
+        os._exit(1)
     
     # Export the 3D feature depending on required format
+    logging.info('Exporting results')
     if ffile == 'csv':
-        gscript.run_command('v.to.points', input=tmp["line3d"], type='line', output=tmp["point3d"], use='vertex')
-        gscript.run_command('v.out.ascii', input=tmp["point3d"], type='point', output='{}/output3d.csv'.format(outdir), format='point', separator='comma', flags='c', overwrite=True)
+        gscript.run_command('v.to.points', input=tmp["line3d"], type='line', output=tmp["point3d"], use='vertex', quiet=True)
+        gscript.run_command('v.out.ascii', input=tmp["point3d"], type='point', output='{}/output3d.csv'.format(outdir), format='point', separator='comma', flags='c', overwrite=True, quiet=True)
     elif ffile == 'kml':
-        gscript.run_command('v.out.ogr', input=tmp["line3d"], output='{}/output3d.kml'.format(outdir) , format='KML', overwrite=True)
+        gscript.run_command('v.out.ogr', input=tmp["line3d"], output='{}/output3d.kml'.format(outdir) , format='KML', overwrite=True, quiet=True)
     elif ffile == 'gml':
-        gscript.run_command('v.out.ogr', input=tmp["line3d"], output='{}/output3d.gml'.format(outdir), format='GML', overwrite=True)
+        gscript.run_command('v.out.ogr', input=tmp["line3d"], output='{}/output3d.gml'.format(outdir), format='GML', overwrite=True, quiet=True)
     elif ffile == 'geojson':
-        gscript.run_command('v.out.ogr', input=tmp["line3d"], output='{}/output3d.geojson'.format(outdir), format='GeoJSON', overwrite=True)
+        gscript.run_command('v.out.ogr', input=tmp["line3d"], output='{}/output3d.geojson'.format(outdir), format='GeoJSON', overwrite=True, quiet=True)
     else:
-        gscript.run_command('v.out.ogr', input=tmp["line3d"], output='{}/output3d.shp'.format(outdir), format='ESRI_Shapefile', overwrite=True)
+        gscript.run_command('v.out.ogr', input=tmp["line3d"], output='{}/output3d.shp'.format(outdir), format='ESRI_Shapefile', overwrite=True, quiet=True)
     
     # Create a zip file with the final output
-    with ZipFile('{}/tmp/output3d.zip'.format(outdir0), 'w') as zipObj:
-        # Iterate over all the files in directory
-        for folderName, subfolders, filenames in os.walk(outdir):
-           for filename in filenames:
-               # Create complete filepath of file in directory
-               filePath = os.path.join(folderName, filename)
-               # Add file to zip
-               zipObj.write(filePath, basename(filePath))
+    logging.info('Preparing the zip file for download')
+    if os.path.exists(outdir) and os.path.isdir(outdir):
+        if os.listdir(outdir):
+            with ZipFile('{}/tmp/output3d.zip'.format(outdir0), 'w') as zipObj:
+                # Iterate over all the files in directory
+                for folderName, subfolders, filenames in os.walk(outdir):
+                   for filename in filenames:
+                       # Create complete filepath of file in directory
+                       filePath = os.path.join(folderName, filename)
+                       # Add file to zip
+                       zipObj.write(filePath, basename(filePath))
+        else:    
+            logging.error('the {} directory is empty'.format(outdir))
+    else:
+        logging.error('the {} directory does not exist'.format(outdir))
+
                
-    # Remove unuseful file like the geopackage adn the tmp file from the grass mapset    
+    # Remove unuseful file like the geopackage adn the tmp file from the grass mapset
+    logging.info('Removing temporary file')
     os.remove('{}/tmp/output.gpkg'.format(outdir0))
     gscript.run_command("g.remove", flags="f", type='raster,vector', name=",".join([tmp[m] for m in tmp.keys()]), quiet=True)
     
